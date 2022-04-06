46694163df91ba5ff3b5656d2714bb29c7ce1741
gateisbug <luftwaffe04@naver.com>
2022-04-06 22:10:39 +0900

    init

--- src/define.ts
+++ src/define.ts
@@ -1,1 +1,34 @@
 export type func = ()=>any
+
+export interface ETF {
+	name: string;
+	ticker: string;
+	explain: string;
+	detail: ETFDetail;
+	tags: string[];
+	link: string;
+}
+
+interface ETFDetail {
+	index: IndexInfo;
+	basic: ETFBasic;
+	Top10: Investment[]
+}
+
+interface IndexInfo {
+	title: string;
+	info: string;
+}
+
+interface ETFBasic {
+	asset: number|string;
+	date: string;
+	fee: number;
+	distribution: boolean;
+}
+
+interface Investment {
+	category: string;
+	ticker: string;
+	size: number;
+}
